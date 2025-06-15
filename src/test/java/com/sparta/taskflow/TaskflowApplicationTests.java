7e3cf425d1d1a88a492764baa27d34971586b733
taehunIM <luxlaetitia9542@gmail.com>
2025-06-15 20:44:08 +0900

    chore: 프로젝트 초기 설정

---
+++ src/test/java/com/sparta/taskflow/TaskflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.taskflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
