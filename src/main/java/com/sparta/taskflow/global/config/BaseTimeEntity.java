b1b77f0e9fac4dcd5c6b8f1c5c31f4dd373e8919
taehunIM <luxlaetitia9542@gmail.com>
2025-06-15 22:09:37 +0900

    BaseTimeEntity 및 JPA Auditing 설정 추가
    BaseTimeEntity 생성 /n- TaskflowApplication에 @EnableJpaAuditing 추가

---
+++ src/main/java/com/sparta/taskflow/global/config/BaseTimeEntity.java
@@ -0,0 +1,26 @@
+package com.sparta.taskflow.global.config;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass // 직접 테이블이 되지않지만 클래스를 상속한 자식 엔티티는 아래 필드들을 컬럼으로 가지게 됨
+@EntityListeners(AuditingEntityListener.class) // JPA 이벤트 리스너 등록
+public  abstract class BaseTimeEntity {
+
+
+    @CreatedDate // 엔티티 최초 저장 시 자동으로 저장 시각 기록
+    @Column(updatable = false) // 초기화 이후 수정 불가
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate // 엔티티가 변경될 때 마다 변경 시각 기록
+    private LocalDateTime updatedAt;
+}
