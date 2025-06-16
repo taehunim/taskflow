d21866ec05cb278205f526efb25ebc07e75ef15d
dain <0901ekdls@gmail.com>
2025-06-16 20:29:28 +0900

    refactor: 컨트롤러 코드 수정 및 ApiResponse 적용
    resolves: #5

--- src/main/java/com/sparta/taskflow/domain/comment/controller/CommentController.java
+++ src/main/java/com/sparta/taskflow/domain/comment/controller/CommentController.java
@@ -3,8 +3,9 @@ package com.sparta.taskflow.domain.comment.controller;
 import com.sparta.taskflow.domain.comment.dto.CommentRequestDto;
 import com.sparta.taskflow.domain.comment.dto.CommentResponseDto;
 import com.sparta.taskflow.domain.comment.service.CommentService;
+import com.sparta.taskflow.response.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,17 +13,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 @RequiredArgsConstructor
-@RequestMapping("/comments")
+@RequestMapping("/api/comments")
 public class CommentController {
 
     private final CommentService commentService;
 
     @PostMapping
-    public ResponseEntity<CommentResponseDto> createComment(
-        @RequestBody CommentRequestDto requestDto) {
-        CommentResponseDto response = commentService.createComment(requestDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    public ResponseEntity<ApiResponse<CommentResponseDto>> createComment(
+        @Valid @RequestBody CommentRequestDto requestDto
+    ) {
+        CommentResponseDto responseDto = commentService.createComment(requestDto);
+        ApiResponse<CommentResponseDto> response = ApiResponse.success(
+            "댓글이 생성되었습니다.",
+            responseDto
+        );
+
+        return ResponseEntity.ok(response);
     }
 }
