de25316a4f91978b34841dee190ddf13c24096c0
kimwonjun1 <monkeydmw@naver.com>
2025-06-18 00:36:31 +0900

    테스ífix : 테스크 생성 api request, response ìdto 수정

--- src/main/java/com/sparta/taskflow/domain/task/dto/request/CreateTaskRequestDto.java
+++ src/main/java/com/sparta/taskflow/domain/task/dto/request/CreateTaskRequestDto.java
@@ -10,10 +10,15 @@ public class CreateTaskRequestDto {
 
     @NotNull(message = "제목은 필수입니다.")
     private String title;
+
     private String description;
+
+    @NotNull(message = "마감일은 필수입니다.")
+    private LocalDateTime dueDate;
+
     @NotNull(message = "우선순위는 필수입니다.")
     private String priority;
+
+    @NotNull(message = "담당자는 필수입니다.")
     private Long assigneeId;
-    private LocalDateTime dueAt;
-    private String status;
 }
