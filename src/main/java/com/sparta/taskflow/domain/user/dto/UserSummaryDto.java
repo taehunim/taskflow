9ac86a180e013fc65346a7c2e200e930bd1713a8
kimwonjun1 <monkeydmw@naver.com>
2025-06-18 00:29:01 +0900

    email 속성 추가

--- src/main/java/com/sparta/taskflow/domain/user/dto/UserSummaryDto.java
+++ src/main/java/com/sparta/taskflow/domain/user/dto/UserSummaryDto.java
@@ -1,11 +1,15 @@
 package com.sparta.taskflow.domain.user.dto;
 
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Builder
 public class UserSummaryDto {
 
     private Long id;
     private String username;
     private String name;
+    private String email;
+
 }
