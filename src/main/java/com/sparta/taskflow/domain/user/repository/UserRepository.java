93d84e7a7fdff442021e2e196cb4461e6ae34178
kimwonjun1 <monkeydmw@naver.com>
2025-06-18 00:33:01 +0900

    findByIdAndIsDeletedFalse 메서드 작성

--- src/main/java/com/sparta/taskflow/domain/user/repository/UserRepository.java
+++ src/main/java/com/sparta/taskflow/domain/user/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.sparta.taskflow.domain.user.repository;
 
 import com.sparta.taskflow.domain.user.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -8,5 +9,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
 
     boolean existsByEmail(String email);
+
+    Optional<User> findByIdAndIsDeletedFalse(Long id);
 
 }
