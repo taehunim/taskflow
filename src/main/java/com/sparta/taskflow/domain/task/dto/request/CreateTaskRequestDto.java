e910edd1ffdd51b6a5e1c8fe7334cc8962ac3474
kimwonjun1 <monkeydmw@naver.com>
2025-06-18 10:39:54 +0900

    fix : CreateTaskRequestDto dueDate, assigneeId NotNull 제거

--- src/main/java/com/sparta/taskflow/domain/task/dto/request/CreateTaskRequestDto.java
+++ src/main/java/com/sparta/taskflow/domain/task/dto/request/CreateTaskRequestDto.java
@@ -13,12 +13,10 @@ public class CreateTaskRequestDto {
 
     private String description;
 
-    @NotNull(message = "마감일은 필수입니다.")
     private LocalDateTime dueDate;
 
     @NotNull(message = "우선순위는 필수입니다.")
     private String priority;
 
-    @NotNull(message = "담당자는 필수입니다.")
     private Long assigneeId;
 }
