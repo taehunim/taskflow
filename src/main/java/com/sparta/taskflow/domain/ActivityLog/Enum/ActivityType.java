47b8b57980998e7958fb4ae94ebf7bb033586839
IM <luxlaetitia9542@gmail.com>
2025-06-18 09:09:01 +0900

    feat: ActivityType enum 생성

---
+++ src/main/java/com/sparta/taskflow/domain/ActivityLog/Enum/ActivityType.java
@@ -0,0 +1,16 @@
+package com.sparta.taskflow.domain.ActivityLog.Enum;
+
+public enum ActivityType {
+
+    TASK_CREATED,
+    TASK_UPDATED,
+    TASK_DELETED,
+    TASK_STATUS_CHANGED,
+
+    COMMENT_CREATED,
+    COMMENT_UPDATED,
+    COMMENT_DELETED,
+
+    USER_LOGGED_IN,
+    USER_LOGGED_OUT
+}
