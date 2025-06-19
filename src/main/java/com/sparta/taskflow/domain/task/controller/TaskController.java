aeb8908e6d80c911efbd6cc1f650c37760168f45
kimwonjun1 <monkeydmw@naver.com>
2025-06-19 15:27:52 +0900

    fix : 테스크 생성 api ê°상태코드 201로 수정

--- src/main/java/com/sparta/taskflow/domain/task/controller/TaskController.java
+++ src/main/java/com/sparta/taskflow/domain/task/controller/TaskController.java
@@ -16,6 +16,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -46,7 +48,7 @@ public class TaskController {
                 responseDto
         );
 
-        return ResponseEntity.ok(response);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping
