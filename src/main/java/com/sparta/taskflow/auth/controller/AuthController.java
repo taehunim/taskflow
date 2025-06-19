ab342b78f50f980e3736649f29a151cf8d928d08
dain <0901ekdls@gmail.com>
2025-06-19 21:24:09 +0900

    fix: 로그인 기능 누락 추가
    resolves: #36

--- src/main/java/com/sparta/taskflow/auth/controller/AuthController.java
+++ src/main/java/com/sparta/taskflow/auth/controller/AuthController.java
@@ -1,8 +1,13 @@
 package com.sparta.taskflow.auth.controller;
 
-import com.sparta.taskflow.auth.service.AuthService;
+import com.sparta.taskflow.auth.dto.request.LoginRequestDto;
 import com.sparta.taskflow.auth.dto.request.RegisterRequestDto;
+
+import com.sparta.taskflow.auth.dto.response.TokenResponse;
+import com.sparta.taskflow.auth.service.AuthService;
+
 import com.sparta.taskflow.domain.user.dto.request.DeleteUserRequestDto;
+
 import com.sparta.taskflow.domain.user.dto.response.UserResponseDto;
 import com.sparta.taskflow.domain.user.service.UserService;
 import com.sparta.taskflow.response.ApiResponse;
@@ -33,14 +38,24 @@ public class AuthController {
         return ResponseEntity.ok(response);
     }
 
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse<TokenResponse>> login(
+        @Valid @RequestBody LoginRequestDto requestDto) {
+        TokenResponse responseDto = authService.login(requestDto.getUsername(),
+            requestDto.getPassword());
+
+        ApiResponse<TokenResponse> response = ApiResponse.success("로그인이 완료되었습니다.", responseDto);
+        return ResponseEntity.ok(response);
+    }
+
+
     // TODO : SpringSecurity 적용 이후 로그인 유저 정보 받는 방법으로 변경 필요.
     @PostMapping("/withdraw")
-    public ResponseEntity<ApiResponse<Void>> withdraw(
-        @RequestParam Long loginUserId,
-        @RequestBody @Valid DeleteUserRequestDto deleteUserDto
-    ) {
+    public ResponseEntity<ApiResponse<Void>> withdraw ( @RequestParam Long loginUserId, @RequestBody @Valid DeleteUserRequestDto deleteUserDto) {
         userService.deleteUser(loginUserId, deleteUserDto);
         ApiResponse<Void> response = ApiResponse.success("회원탈퇴가 완료되었습니다.", null);
+
         return ResponseEntity.ok(response);
     }
 }
