1826d76963ef9914fc10da9caa039f190504f6b4
천세경 <jay45652@gmail.com>
2025-06-20 09:51:10 +0900

    refactor: 사용하지 않는 메소드 삭제

--- src/main/java/com/sparta/taskflow/domain/activitylog/repository/ActivityLogRepository.java
+++ src/main/java/com/sparta/taskflow/domain/activitylog/repository/ActivityLogRepository.java
@@ -1,22 +1,11 @@
 package com.sparta.taskflow.domain.activitylog.repository;
 
-import com.sparta.taskflow.domain.activitylog.Enum.ActivityType;
 import com.sparta.taskflow.domain.activitylog.entity.ActivityLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.time.LocalDateTime;
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ActivityLogRepository extends JpaRepository<ActivityLog, Long> {
 
     List<ActivityLog> findAllByUserId(Long userId);
 
-    List<ActivityLog> findAllByUserIdAndActivityType(Long userId, ActivityType activityType);
-
-    List<ActivityLog> findAllByUserIdAndTargetId(Long userId, Long targetId);
-
-    List<ActivityLog> findAllByUserIdAndCreatedAtBetween(
-            Long userId,
-            LocalDateTime startAt,
-            LocalDateTime endAt);
 }
